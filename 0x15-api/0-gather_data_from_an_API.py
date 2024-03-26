d09f70ea9811154c2f67976b5d12d78f4ff2694b
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-03-26 04:30:40 +0200

    task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,8 +11,12 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     employee_id = sys.argv[1]
 
-    user_response = requests.get(url + "users/{}".format(employee_id))
-    todos_response = requests.get(url + "todos", params={"userId": employee_id})
+    user_response = requests.get(
+        url + "users/{}".format(employee_id)
+    )
+    todos_response = requests.get(
+        url + "todos", params={"userId": employee_id}
+    )
 
     if user_response.status_code != 200 or todos_response.status_code != 200:
         print("Failed to fetch data. Please try again later.")
@@ -24,7 +28,9 @@ if __name__ == "__main__":
     completed_tasks = [task for task in todos if task.get('completed')]
 
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed_tasks), len(todos)))
+        user.get("name"), len(completed_tasks), len(todos)
+    ))
 
     for task in completed_tasks:
         print("\t{}".format(task.get("title")))
+
