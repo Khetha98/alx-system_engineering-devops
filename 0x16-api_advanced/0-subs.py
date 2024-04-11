b897893af5dd3d80d208594542b5921efa2722f4
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-04-11 23:47:09 +0200

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,3 +22,12 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        print(number_of_subscribers(subreddit))
