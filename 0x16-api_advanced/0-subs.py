c039170ecd6e99efa7790d2c7639b7f9d8fbb209
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-04-12 00:02:39 +0200

    files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,16 +6,15 @@
 import requests
 
 
-def number_of_subscribers(subreddit, after=None):
+def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit.
     If the subreddit is invalid, returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"after": after} if after else {}
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,16 +6,15 @@
 import requests
 
 
-def top_ten(subreddit, after=None):
+def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts listed for a given subreddit.
     If the subreddit is invalid, prints None.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"after": after} if after else {}
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
