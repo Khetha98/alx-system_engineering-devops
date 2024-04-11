7b6966c03990dc0feee960f2cd311cadac916c1b
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-04-11 21:41:42 +0200

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+It contains number_of_subscribers function
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """It returns number of subscribers for the given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    i = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = i.get("data", {}).get("subscribers", 0)
+    return subs
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+These are advanced API project
