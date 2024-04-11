abd08a106fc253bb342eceeabe0b437bc2e71665
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-04-11 23:44:49 +0200

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
-
 """
-query the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
+1-top_ten
 """
 
-from requests import get
-from sys import argv
+import requests
 
 
-def top_ten(subreddit: str) -> None:
+def top_ten(subreddit):
     """
-    function that does the heavy lifting for us
-    Args:
-        subreddit (str) -> The subreddit to query
-    Returns: The top 10 hots for that subreddit
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    If the subreddit is invalid, prints None.
     """
-    headers = {
-        "User-Agent": "Marvel's Agents of Shield/21",
-        "X-Forwared-For": "Phil J. Coulson"
-    }
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-    request_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=headers)
 
-    try:
-        response = get(request_url, headers=headers,
-                       allow_redirects=False).json()
-        data = response['data']['children']
-        [print(post['data']['title']) for post in data[:10]]
-    except Exception:
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+        for post in posts:
+            print(post["data"]["title"])
+    else:
         print("None")
 
-
 if __name__ == "__main__":
-    (top_ten(argv[1]))
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        top_ten(subreddit)
