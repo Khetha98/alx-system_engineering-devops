2009957f9376d02708422e3ffd2c937582ff75b8
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-04-17 15:35:49 +0200

    file

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Script that increase Nginx traffic
+
+# Changes ULIMIT variable from default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Nginx is restarted
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
+
