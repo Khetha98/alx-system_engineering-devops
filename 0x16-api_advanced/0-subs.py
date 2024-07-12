9872220d8da4a0356a5e0ad65679be534f1c70b5
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-07-12 17:15:35 +0200

    reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,23 +5,13 @@ Contains the number_of_subscribers function
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    if not isinstance(subreddit, str) or not subreddit:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {
-        'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'
-    }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        # Check if subreddit does not exist
-        if response.status_code == 404:
-            return 0
-        # If request is successful, extract the number of subscribers
-        response_data = response.json()
-        return response_data.get("data", {}).get("subscribers", 0)
-    except requests.RequestException:
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
