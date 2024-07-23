57178babdd2c499c842d5ec806961843db244650
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-07-23 22:13:47 +0200

    user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def number_of_subscribers(subreddit) -> int:
+def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers
 
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit) -> int:
     # Returns the number of subscribers for a given subreddit
     base_url = f"https://www.reddit.com/r/{subreddit}/about.json"
     try:
-        request = requests.get(base_url, headers={'User-Agent': 'Agent Uche'}, allow_redirects=False)
+        request = requests.get(base_url, headers={'User-Agent': 'Agent Uche'})
         data = request.json()
         return data['data']['subscribers']
     except Exception:
