3b0b50ee1001c1de3bb36a9ccf594ff74f7666ef
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-07-23 20:57:19 +0200

    modification

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
-
+"""
+This has number_of_subscribers function
+"""
 from requests import get
-
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
+    function which queries the Reddit API and returns the
+    number of subscribers
     (not active users, total subscribers) for a given subreddit.
     """
 
