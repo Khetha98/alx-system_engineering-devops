71bb76394b01138c712a9aec48627837c1d3c93a
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-08-15 22:05:49 +0200

    removed main files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,6 @@ of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
-import sys
 
 
 def top_ten(subreddit):
@@ -26,10 +25,3 @@ def top_ten(subreddit):
             print(title)
     else:
         print(None)
-
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
