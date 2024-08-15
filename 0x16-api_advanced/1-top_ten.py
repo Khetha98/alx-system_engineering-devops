3f90f331d86653d853e7d126671b0c129e826fd3
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-08-15 22:44:39 +0200

    number 1 remove redirect

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,6 +6,7 @@ of the first 10 hot posts listed for a given subreddit.
 
 import requests
 
+
 def top_ten(subreddit):
     """
     Function that queries the Reddit API.
@@ -14,23 +15,13 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"limit": 10}
-    
-    try:
-        req = requests.get(url, headers=headers, params=params)
-        if req.status_code == 200:
-            try:
-                data = req.json()
-                posts = data.get("data", {}).get("children", [])
-                if not posts:
-                    print(None)
-                    return
-                for post in posts:
-                    title = post["data"].get("title")
-                    if title:
-                        print(title)
-            except ValueError:
-                print(None)
-        else:
-            print(None)
-    except requests.RequestException:
+
+    req = requests.get(url, headers=headers, params=params)
+
+    if req.status_code == 200:
+        for get_data in req.json()["data"]["children"]:
+            dat = get_data["data"]
+            title = dat["title"]
+            print(title)
+    else:
         print(None)
