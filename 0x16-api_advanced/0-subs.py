7daa1b3456a810c1145bd2a2626b9bcbb8692d92
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-09-10 14:29:10 +0200

    another trial

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit
-"""
-
+"""Function"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Return the total number of subscribers on a given subreddit
-    the active subscribers.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, allow_redirects=False)
-
-    if response.status_code == 200:
-
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-
-        return subscribers
-    else:
+    """Return"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
